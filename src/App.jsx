f4c027180f625e8b7cdd814b7bcc63806409c3ae
Mateusz <matimatik97@gmail.com>
2020-08-21 17:20:17 +0200

    DEV-002 - Improve login and access token storage

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,28 @@ import {
   setAccessTokenCookies,
   getAccessTokenFromCookies,
 } from "./utils/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = SpotifyWebApi();
 
 const App = () => {
   const [token, setToken] = useState(null);
 
   useEffect(() => {
-    const { accessTokenFromUrl, expiresIn } = getAccessTokenFromUrl();
-    window.location.hash = "";
+    (async () => {
+      // login with "LOGIN WITH SPOTIFY" button
+      const { accessTokenFromUrl, expiresIn } = getAccessTokenFromUrl();
+      if (accessTokenFromUrl) {
+        setToken(accessTokenFromUrl);
+        setAccessTokenCookies(accessTokenFromUrl, expiresIn);
+      }
 
-    // login with "LOGIN WITH SPOTIFY" button
-    if (accessTokenFromUrl) {
-      setToken(accessTokenFromUrl);
-      setAccessTokenCookies(accessTokenFromUrl, expiresIn);
-    }
-
-    const accessTokenFromCookies = getAccessTokenFromCookies();
-    if (accessTokenFromCookies) {
-      setToken(accessTokenFromCookies);
-    }
-
-    // login with access token if not expired
+      // login with access token if not expired
+      const accessTokenFromCookies = await getAccessTokenFromCookies();
+      if (accessTokenFromCookies) {
+        setToken(accessTokenFromCookies);
+      }
+    })();
   }, []);
 
   return <div className="app">{token ? <h1>Logged In</h1> : <Login />}</div>;
--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -18,6 +18,8 @@ export const getAccessTokenFromUrl = () => {
     const accessTokenFromUrl = urlParams.get("access_token");
     const expiresIn = urlParams.get("expires_in");
 
+    window.location.hash = "";
+
     return { accessTokenFromUrl, expiresIn };
 }
 
@@ -28,14 +30,34 @@ export const setAccessTokenCookies = (accessToken, expiresIn) => {
     document.cookie = `spotify_access_token=${accessToken}; expires=${date.toUTCString()}; path=/`;
 }
 
-export const getAccessTokenFromCookies = () => {
-    if (document.cookie.length > 0) {
-        const urlParams = new URLSearchParams(`?${document.cookie.replace("; ", "&")}`);
-        
-        return urlParams.get("spotify_access_token");
-    }
+export const getAccessTokenFromCookies = async () => {
+    if (document.cookie.length === 0) return null;
 
-    return null;
+    const urlParams = new URLSearchParams(`?${document.cookie.replace(/(; )/g, "&")}`);
+
+    const accessTokenFromCookies = urlParams.get("spotify_access_token");
+
+    try {
+        const response = await fetch('https://api.spotify.com/v1/me', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessTokenFromCookies}`
+            },
+            referrerPolicy: 'no-referrer',
+        });
+
+        if (response.ok) {
+            // console.log("ok");
+            return accessTokenFromCookies;
+        } else {
+            throw new Error("Wrong access token...");
+        }
+    } catch (error) {
+        // console.log("not ok");
+        return null;
+    }
 }
 
 export const loginUrl = `${authEndpoint}?response_type=token&show_dialog=true&client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${encodeURIComponent(scopes)}`;
