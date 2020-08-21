6be965e6c0410b9a9497a9724c9a34dd313b01d2
Mateusz <matimatik97@gmail.com>
2020-08-22 00:19:17 +0200

    DEV-006 - Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,49 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__left">
-        <h2>Left</h2>
+        <img
+          src="https://i.imgflip.com/2flvc1.jpg"
+          alt="footer-album-logo"
+          className="footer__albumLogo"
+        />
+        <div className="footer__songInfo">
+          <h4>Hotline Bling</h4>
+          <p>Drake</p>
+        </div>
       </div>
+
       <div className="footer__center">
-        <h2>Center</h2>
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
       </div>
+
       <div className="footer__right">
-        <h2>Right</h2>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
